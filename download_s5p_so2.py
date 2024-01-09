0662118e92d68e90a7d6b0b00077af66348d7396
raasdafsa <raasdafsa@gmail.com>
2024-01-09 15:59:25 +1100

    downloader for so2

---
+++ download_s5p_so2.py
@@ -0,0 +1,96 @@
+import os
+import hashlib
+from pathlib import Path, PurePath
+import shutil
+import datetime
+import click
+import glob
+from utilities import get_logger
+import ee
+import geetools
+from datetime import date, timedelta
+import json
+
+
+
+today = date.today() - timedelta(days=1)
+yesterday = str(today - timedelta(days=1))
+
+
+
+@click.command()
+@click.option('--drive-folder', default="S5P_SO2")
+@click.option('--from-date', default=yesterday)
+@click.option('--to-date', default=str(today))
+@click.option('--log-dir', default="logs")
+@click.option('--service-account', envvar='AIRMOVE_SERVICE_ACCOUNT', default=None)
+@click.option('--service-file', envvar='AIRMOVE_SERVICE_FILE',default=None)
+@click.option('--ncr-bounds-file', envvar='NCR_BOUNDS_FILE', default='shared/NCRBoundary.json')
+def download_s5p_so2(
+    drive_folder, from_date, to_date, log_dir,
+    service_account, service_file, ncr_bounds_file
+):
+
+    logger = get_logger(
+        script_name="download_s5p_so2",
+        log_directory=log_dir
+    )
+    def _info(message):
+        click.echo(message)
+        logger.info(message)
+    def _error(message):
+        click.echo(message)
+        logger.error(message)
+    
+    if not service_account or not service_file:
+        _error("Service account or service file is invalid.")
+        exit(1)
+
+    credentials = ee.ServiceAccountCredentials(service_account, service_file)
+    ee.Initialize(credentials)
+    _info("Service account ok.")
+
+    _info(f"S5P_SO2 processing from: {from_date} to {to_date}.")
+
+    philippines = ee.Geometry.Polygon(
+        [[[116.71687839753203, 18.744929081691417],
+          [116.71687839753203, 5.019622515938524],
+          [126.91219089753203, 5.019622515938524],
+          [126.91219089753203, 18.744929081691417]]], None, False)
+
+
+    ncr_box = ee.Geometry.Polygon(
+        [[[120.88626682729029, 14.33788811927746],
+          [121.15543186635279, 14.33788811927746],
+          [121.15543186635279, 14.792458281417941],
+          [120.88626682729029, 14.792458281417941],
+          [120.88626682729029, 14.33788811927746]]], None, False)
+
+    with open(ncr_bounds_file) as f:
+        ncr_shape = json.load(f)
+    ncr_shape = ee.FeatureCollection(ncr_shape)
+    ncr_bound = ncr_shape.geometry()
+
+    so2 = ee.ImageCollection('COPERNICUS/S5P/NRTI/L3_SO2').select('SO2_column_number_density').filterBounds(ncr_shape).filterDate(from_date, to_date)
+    val_max = 0.0002
+    val_min = 0
+    band_viz = {
+        "min": val_min,
+        "max": val_max, 
+        "opacity": 1.0, 
+        "palette": ["black","blue","purple","cyan","green","yellow","red"]
+    }
+
+    NCR_mean = so2.mean()
+    
+    geetools.batch.Export.imagecollection.toDrive(
+        collection=so2,
+        namePattern="S5P_SO2_{system_date}",
+        folder=drive_folder,
+        region=ncr_box,
+        verbose=True)
+
+
+
+if __name__ == '__main__':
+    download_s5p_so2()
